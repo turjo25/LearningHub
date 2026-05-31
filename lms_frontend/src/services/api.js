0c286ba3232339aec7bc4ece8fc3a3ace9dfa6e2
turjo25 <turjorahman556@gmail.com>
2026-05-31 18:48:50 +0600

    Configure dynamic API URL for frontend production environment

--- lms_frontend/src/services/api.js
+++ lms_frontend/src/services/api.js
@@ -4,7 +4,7 @@
  */
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/api";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/api";
 
 const api = axios.create({
   baseURL: API_URL,
